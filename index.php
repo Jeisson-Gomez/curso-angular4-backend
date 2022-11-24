06038d44436c030205386268c0e5952a07fb4978
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-11-24 16:46:14 -0500

    Actualizacion del proyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use \Slim\App;
+
+$app = new \Slim\App($app);
+
+$app-> get("/pruebas", function() use($app){
+    echo "Hola mundo desde Slim PHP";
+});
+
+$app-> run();
