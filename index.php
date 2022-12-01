a16c0cc6d382e64f74ed666ed512705dfb21bfe1
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-12-01 17:11:53 -0500

    Actualizaion del proyecto

--- index.php
+++ index.php
@@ -9,4 +9,8 @@ $app-> get("/pruebas", function() use($app){
     echo "Hola mundo desde Slim PHP";
 });
 
+$app-> get("/pruebas", function() use($app){
+    echo "Otro texto Cualquiera";
+});
+
 $app-> run();
