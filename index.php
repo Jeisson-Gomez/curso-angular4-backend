06ba7257c09d6914fed5cfc3937b3339ae40df54
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-12-02 20:25:59 -0500

    Actualizacion del proyecto

--- index.php
+++ index.php
@@ -2,15 +2,48 @@
 
 require_once 'vendor/autoload.php';
 use \Slim\App;
-
+$db = new mysqli('localhost', 'root', '', 'curso_angular4');
 $app = new \Slim\App($app);
 
-$app-> get("/pruebas", function() use($app){
+$app->get("/pruebas", function() use($app, $db){
     echo "Hola mundo desde Slim PHP";
 });
 
-$app-> get("/pruebas", function() use($app){
+$app->get("/pruebas", function() use($app){
     echo "Otro texto Cualquiera";
+});
+
+$app->post('/productos', function() use($app, $db){
+    $json = $app->request->post('json');
+    $data = json_decode($json, true);
+
+    var_dump($json);
+    var_dump($data);
+
+    if(!isset($data['nombre'])){
+        $data['nombre'] = NULL;
+    }
+
+    if(!isset($data['descripcion'])){
+        $data['descripcion'] = NULL;
+    }
+
+    if(!isset($data['precio'])){
+        $data['precio'] = NULL;
+    }
+
+    if(!isset($data['imagen'])){
+        $data['imagen'] = null;
+    }
+
+    $query = "Insert Into productos values(NULL,".
+            "'{$data['nombre']}'".
+            "'{$data['descripcion']}'".
+            "'{$data['precio']}'".
+            "'{$data['imagen']}'".
+            ")";
+    
+    var_dump($query);
 });
 
 $app-> run();
